0e962549d135672cd5bb62267605621a9528a369
dy <995743253@qq.com>
2026-09-08 21:30:44 +0800

    chore: ignore local diagnostic scripts

--- check-eventlog.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'SilentlyContinue'
-$events = Get-WinEvent -FilterHashtable @{ LogName = 'Application'; StartTime = (Get-Date).AddMinutes(-20) } -MaxEvents 30
-foreach ($event in $events) {
-    if ($event.Message -match 'AiDataGateway' -or $event.ProviderName -match '\.NET') {
-        Write-Output ('--- ' + $event.TimeCreated.ToString('HH:mm:ss') + ' [' + $event.ProviderName + '] level=' + $event.Level)
-        Write-Output ($event.Message.Substring(0, [Math]::Min(900, $event.Message.Length)))
-    }
-}
